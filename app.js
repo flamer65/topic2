7d80081f1ea4c581bae1dab0b943e97683fabb17
flaming65 <namank6510@gmail.com>
2023-02-09 12:03:55 +0530

    css

---
+++ app.js
@@ -0,0 +1,23 @@
+const devices = [];
+
+devices.push({ user: "Mary", name: "Mary's iPhone" });
+devices.push({ user: "Alex", name: "Alex's Surface Pro" });
+devices.push({ user: "Mary", name: "Mary's MacBook" });
+
+devices.forEach(function(device) {
+    const table = document.querySelector('#devices');
+    const row = document.createElement('tr');
+
+    const user = document.createElement('td');
+    const userText = document.createTextNode(device.user);
+    user.appendChild(userText);
+
+    const name = document.createElement('td');
+    const nameText = document.createTextNode(device.name);
+    name.appendChild(nameText);
+
+    row.appendChild(user);
+    row.appendChild(name);
+
+    table.appendChild(row);
+});
