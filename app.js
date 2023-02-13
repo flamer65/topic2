e4b9062d176c761669a8bcaa696f59a1cd528c43
flaming65 <namank6510@gmail.com>
2023-02-13 19:48:40 +0530

    js

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
-const devices = [];
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
-devices.push({ user: "Mary", name: "Mary's iPhone" });
-devices.push({ user: "Alex", name: "Alex's Surface Pro" });
-devices.push({ user: "Mary", name: "Mary's MacBook" });
+
+// devices.forEach(function(device) {
+//     const table = document.querySelector('#devices');
+//     const row = document.createElement('tr');
+
+//     const user = document.createElement('td');
+//     const userText = document.createTextNode(device.user);
+//     user.appendChild(userText);
+
+//     const name = document.createElement('td');
+//     const nameText = document.createTextNode(device.name);
+//     name.appendChild(nameText);
+
+//     row.appendChild(user);
+//     row.appendChild(name);
+
+//     table.appendChild(row);
+//   });
 
 devices.forEach(function(device) {
-    const table = document.querySelector('#devices');
-    const row = document.createElement('tr');
+    $('#devices tbody').append(`
+      <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+      </tr>`);
+});
 
-    const user = document.createElement('td');
-    const userText = document.createTextNode(device.user);
-    user.appendChild(userText);
-
-    const name = document.createElement('td');
-    const nameText = document.createTextNode(device.name);
-    name.appendChild(nameText);
-
-    row.appendChild(user);
-    row.appendChild(name);
-
-    table.appendChild(row);
+$('#add-device').on('click', function() {
+    const user = $('#user').val();
+    const name = $('#name').val();
+    devices.push({ user, name });
+    console.log(devices);
+    localStorage.setItem('devices', JSON.stringify(devices));
+    location.href = 'device-list.html';
 });
